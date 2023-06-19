dfba619ad554f1b9d94d5d2a4c33521d1be1f117
Alexandr <tulchan@yandex.ru>
2023-06-19 12:34:18 +0300

    mesto project

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+let closeButton = document.querySelector(".popup__close-button");
+let popup = document.querySelector(".popup");
+let editButton = document.querySelector(".profile__edit-button");
+let profileNameInput = document.querySelector(".popup__input_type_username");
+let profileStatusInput = document.querySelector(
+  ".popup__input_type_userstatus"
+);
+let profileName = document.querySelector(".profile__name");
+let profileStatus = document.querySelector(".profile__status");
+let popupForm = document.querySelector(".popup__form");
+
+function openPopup() {
+  profileNameInput.value = profileName.textContent;
+  profileStatusInput.value = profileStatus.textContent;
+
+  popup.classList.add("popup__opened");
+}
+
+function closePopup() {
+  popup.classList.remove("popup__opened");
+}
+
+function saveProfile(event) {
+  event.preventDefault();
+  profileName.textContent = profileNameInput.value;
+  profileStatus.textContent = profileStatusInput.value;
+
+  closePopup();
+}
+
+document.addEventListener("click", (e) => {
+  if (e.target === popup) {
+    popup.classList.remove("popup__opened");
+  }
+});
+
+editButton.addEventListener("click", openPopup);
+
+closeButton.addEventListener("click", closePopup);
+
+popupForm.addEventListener("submit", saveProfile);
