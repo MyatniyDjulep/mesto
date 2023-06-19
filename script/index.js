79ef764ab48d30245c88d1ff9634b1fadd73f69e
Alexandr <tulchan@yandex.ru>
2023-06-19 20:38:06 +0300

    nightmare

--- script/index.js
+++ script/index.js
@@ -1,23 +1,23 @@
-let closeButton = document.querySelector(".popup__close-button");
-let popup = document.querySelector(".popup");
-let editButton = document.querySelector(".profile__edit-button");
-let profileNameInput = document.querySelector(".popup__input_type_username");
+let closeButton = document.querySelector('.popup__close-button');
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
+let profileNameInput = document.querySelector('.popup__input_type_username');
 let profileStatusInput = document.querySelector(
-  ".popup__input_type_userstatus"
+  '.popup__input_type_userstatus'
 );
-let profileName = document.querySelector(".profile__name");
-let profileStatus = document.querySelector(".profile__status");
-let popupForm = document.querySelector(".popup__form");
+let profileName = document.querySelector('.profile__name');
+let profileStatus = document.querySelector('.profile__status');
+let popupForm = document.querySelector('.popup__form');
 
 function openPopup() {
   profileNameInput.value = profileName.textContent;
   profileStatusInput.value = profileStatus.textContent;
 
-  popup.classList.add("popup_opened");
+  popup.classList.add('popup_opened');
 }
 
 function closePopup() {
-  popup.classList.remove("popup_opened");
+  popup.classList.remove('popup_opened');
 }
 
 function saveProfile(event) {
@@ -28,14 +28,14 @@ function saveProfile(event) {
   closePopup();
 }
 
-document.addEventListener("click", (e) => {
+document.addEventListener('click', (e) => {
   if (e.target === popup) {
-    popup.classList.remove("popup_opened");
+    popup.classList.remove('popup_opened');
   }
 });
 
-editButton.addEventListener("click", openPopup);
+editButton.addEventListener('click', openPopup);
 
-closeButton.addEventListener("click", closePopup);
+closeButton.addEventListener('click', closePopup);
 
-popupForm.addEventListener("submit", saveProfile);
+popupForm.addEventListener('submit', saveProfile);
